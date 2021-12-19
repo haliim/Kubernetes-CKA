1f625dc13039ba4f81b8df2537e55c4162c0d660
Ahmed Halim <ah.hesham93@gmail.com>
2021-12-19 18:30:41 +0200

    finalizing ReplicaSets

---
+++ commands.sh
@@ -0,0 +1,6 @@
+kubectl get pods / replicationcontroller / replicaset
+kubectl delete pod / replicationcontroller anelapod
+kubectl replace 
+kubectl describe
+kubectl edit
+kubectl scale replicaset name --replicas=2
