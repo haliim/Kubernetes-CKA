2b26b93de2235bec4baa5a99a5fdf328a56ffe55
Ahmed HEsham AbdElHalim <32435138+haliim@users.noreply.github.com>
2021-12-21 15:27:36 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -4,3 +4,14 @@ kubectl replace
 kubectl describe
 kubectl edit
 kubectl scale replicaset name --replicas=2
+
+#===================
+Deployments Commands
+
+kubectl create -f file.yaml
+kubectl get deployments
+kubectl apply -f file.yaml # upraing eployment by changing the yaml file config
+kubectl set image deployment/myapp-deployment nginx=nginx:1.7.1
+kubectl rollout status deployment/myapp-deployment
+kubectl rollout history deployment/myapp-deployment
+kubectl rollout undo deployment/myapp-deployment
