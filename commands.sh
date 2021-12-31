e313df63dd862214956888947154e53901acc566
Ahmed Halim <ah.hesham93@gmail.com>
2021-12-31 22:51:38 +0200

    intro to scheduling

--- commands.sh
+++ commands.sh
@@ -24,4 +24,9 @@ kubectl get pods --all-namespaces
 ubectl create -f file.yaml --namespace=xxx # or you can add namspace under metadata section
 kubectl create namespace xxx
 kubectl config set-context $(kubectl config current-context) --namespace=dev
+db-service.dev.svc.cluster.local == name.namspace.type.cluster.local
 
+#=========================
+Imperative & Declarative
+kubectl expose pod redis --port=6379 --name redis-service
+kubectl create deployment webapp --image=kodekloud/webapp-color --replicas=3
